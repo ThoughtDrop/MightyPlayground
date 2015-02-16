fdb7e92d1f58436aea175a26b84c675335adfbe6
Robert Ing <rmi22186@gmail.com>
2015-02-16 15:44:52 -0800

    (feat) Update user auth controller and routes

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -3,6 +3,6 @@ var authController = require('./authController.js');
 module.exports = function (app) {
 
   app.post('/', authControllerController.findUser); 
-  app.delete('/', authControllerController.delete);
+  app.delete('/', authController.delete);
   app.post('/phone', authControllerController.getPhoneNumber);
 };
