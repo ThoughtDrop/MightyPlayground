8d553386b03fafa0d4fccba5f96ec4030f6a1818
Robert Ing <rmi22186@gmail.com>
2015-02-16 15:17:51 -0800

    (feat) Add delete to auth controller

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -2,6 +2,7 @@ var authController = require('./authController.js');
 
 module.exports = function (app) {
 
-  app.get('/', authControllerController.findUser);
-
+  app.post('/', authControllerController.findUser); 
+  app.delete('/', authControllerController.delete);
+  app.post('/phone', authControllerController.getPhoneNumber);
 };
