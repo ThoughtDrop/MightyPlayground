5de58eab6e00b3397e8108c499b29f8e327fd487
Peter Kim <chong.peter.kim@gmail.com>
2015-02-16 15:27:29 -0800

    (feat) Added messageController & messageRoutes)

--- server/messages/messageController.js
+++ server/messages/messageController.js
@@ -1,14 +1,41 @@
 var Message = require('../../db/models/messages.js');
+var Q = require('q');
 
 module.exports = {
 
   create: function (req, res) {
-    var newMessage = req.body;
-    
-  },
+    var createMessage = Q.nbind(Message.create, Message);
 
+    var data = {
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      radius: req.body.radius,
+      created_by: req.body.created_by
+    };
+
+    console.log(data);
+
+    createMessage(data) 
+      .then(function (createdMessage) {
+        res.send('saved message!');
+      })
+      .fail(function (error) {
+        next(error);
+      });
+
+
+  },
+    //this gets all data points for now. Will have to refactor once we know how the db will work.
   fetch: function(req, res) {
-    
+    var findAll = Q.nbind(Message.find, Message);
+
+    findAll({})
+      .then(function (messages) {
+        res.json(messages);
+      })
+      .fail(function (error) {
+        next(error);
+      });
   }
 
 };
