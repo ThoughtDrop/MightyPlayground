dd18538760b47fdeac204a0ec7afaba8cd05e7b9
Peter Kim <chong.peter.kim@gmail.com>
2015-02-18 14:46:45 -0800

    (feat) Retrieve user profile data on login. Need to refactor & store user data in DB

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,16 +27,37 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('AuthCtrl', function($scope, $cordovaOauth, $location){
+.controller('AuthCtrl', function($scope, $cordovaOauth, $location, $localStorage, $http){
 
   $scope.login = function() {
       $cordovaOauth.facebook(427819184047831, []).then(function(result) {
-          // $localStorage.accessToken = result.access_token;
-          console.log(JSON.stringify(result));
+          $localStorage.accessToken = result.access_token;
+          console.log('result: ' + JSON.stringify(result));
+
+          $scope.init();
+
           $location.path("/tab/messages");
       }, function(error) {
           alert("There was a problem signing in!  See the console for logs");
           console.log(error);
       });
+
+
   };
+
+  $scope.init = function() {
+          if($localStorage.hasOwnProperty("accessToken") === true) {
+              $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
+                  $scope.profileData = result.data;
+                  console.log('init!');
+                  console.log(JSON.stringify(result.data));
+              }, function(error) {
+                  alert("There was a problem getting your profile.  Check the logs for details.");
+                  console.log(error);
+              });
+          } else {
+              alert("Not signed in");
+              $location.path("/login");
+          }
+      };
 });
