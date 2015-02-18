c53bddb40f034747e816d1a2438279d51745da3c
Ryan McCarter <mccarter.ryan@gmail.com>
2015-02-18 15:27:04 -0800

    Gets user location and fetches messages around user

--- server/messages/messageController.js
+++ server/messages/messageController.js
@@ -1,21 +1,26 @@
 var Message = require('../../db/models/messages.js');
 var Q = require('q');
+
 module.exports = {
 
   findAround: function(req, res) {
-    // var findAround = Q.nbind(Message.find, Message);
+    var findAround = Q.nbind(Message.find, Message);
+    console.log(req.body.long, req.body.lat);
+
     var query = {};
     query.location = {
       $near : {
         $geometry : {
           type : "Point",
-          coordinates : [-122.408995, 37.783624] //TODO - modify to use own coordinates on app open
+          coordinates : [req.body.long, req.body.lat] 
         },
         $maxDistance : 100
       }
     };
     Message.find(query, function(err, result){
+      console.log('DATABASE RESULT', result);
       res.send(result);
+      // res.json(result);
     });
   },
 
