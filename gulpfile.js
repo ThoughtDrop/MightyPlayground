a8be2b946baab7ab765603f4b30499ad02140f26
Henry Wong <core.safecrlf>
2015-02-19 20:20:51 -0800

    (housekeeping) gulpfile reloads page

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,37 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var nodemon   = require('gulp-nodemon');
+var bs = require('browser-sync');
+var reload = bs.reload;
+
+// var gutil = require('gulp-util');
+// var concat = require('gulp-concat');
+// var sass = require('gulp-sass');
+// var minifyCss = require('gulp-minify-css');
+// var rename = require('gulp-rename');
+// var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  //sass: ['./scss/**/*.scss'],
+  scripts: ['www/js/*.js'],
+  html: ['www/templates/*.html', 'www/index.html'],
+  styles: ['www/css/*.css'],
+  test: ['specs/**/*.js']
 };
 
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+gulp.task('start', ['serve'],function () {
+  bs({
+    notify: true,
+    // address for server,
+    injectChanges: true,
+    files: paths.scripts.concat(paths.html, paths.styles),
+    proxy: 'localhost:8000'
+  });
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+// start our node server using nodemon
+gulp.task('serve', function() {
+  nodemon({script: 'server/server.js', ignore: 'node_modules/**/*.js'});
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
+gulp.task('default', ['start']);
