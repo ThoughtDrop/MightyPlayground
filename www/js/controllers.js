f2f7223bf3c16383658ddd3b1ba277ee9b7b102d
Robert Ing <rmi22186@gmail.com>
2015-02-19 23:46:12 -0800

    (housekeeping) Remove several templates, untabbed lots of code, removed extra lines, added inject strings to gulp for production

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,4 @@
-angular.module('starter.controllers', [])
-
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+angular.module('thoughtdrop.controllers', [])
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
@@ -32,46 +11,41 @@ angular.module('starter.controllers', [])
   $scope.data = {};
 
   $scope.login = function() {
-      $cordovaOauth.facebook(427819184047831, []).then(function(result) {
-          $localStorage.accessToken = result.access_token;
 
-          window.localStorage.token = result.access_token; //store token locally
-          Facebook.storeId($scope.init()); //store id in db
-          $location.path("/phone"); // redirect to phone number input
+    $cordovaOauth.facebook(427819184047831, []).then(function(result) {
+      $localStorage.accessToken = result.access_token;
 
-      }, function(error) {
-          alert("There was a problem signing in!  See the console for logs");
-          console.log(error);
-      });
+      window.localStorage.token = result.access_token; //store token locally
+      Facebook.storeId($scope.init()); //store id in db
+      $location.path("/phone"); // redirect to phone number input
 
-
+    }, function(error) {
+      alert("There was a problem signing in!  See the console for logs");
+      console.log(error);
+    });
   };
 
   $scope.init = function() {
-      console.log('init');
-          if($localStorage.hasOwnProperty("accessToken") === true) {
-              $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
-                  $scope.profileData = result.data;
-                  $scope.data.id = result.data.id;
+    if($localStorage.hasOwnProperty("accessToken") === true) {
+      $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
+        $scope.profileData = result.data;
+        $scope.data.id = result.data.id;
 
-                  return result.data.id;
-              }, function(error) {
-                  alert("There was a problem getting your profile.  Check the logs for details.");
-                  console.log(error);
-              });
-          } else {
-              alert("Not signed in");
-              //$location.path("/login");
-          }
-      };
+        return result.data.id;
+      }, function(error) {
+        alert("There was a problem getting your profile.  Check the logs for details.");
+        console.log(error);
+      });
+    } else {
+      alert("Not signed in");
+      $location.path("/login");
+    }
+  };
 
   $scope.updatePhone = function() {
     Facebook.updatePhone($scope.data);
       //check if id & # mathches for returning users in db
         //otherwise redirect to /login
-
-    $location.path('/tab/messages')
-  };     
-
-  
+    $location.path('/tab/messages');
+  };
 });
