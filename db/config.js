ef1ad29aee2b0964d7b73ebf095ccf5150aa646e
Robert Ing <rmi22186@gmail.com>
2015-02-20 11:27:49 -0800

    (housekeeping) make db dev mode

--- db/config.js
+++ db/config.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 // var keys = require('../keys.js');
 
 // if in production:
-mongoose.connect('mongodb://mightyplayground:hr23thesis@ds043991.mongolab.com:43991/mpmongolab');
-// mongoose.connect('mongodb://localhost/mightyPlayground');
+// mongoose.connect('mongodb://mightyplayground:hr23thesis@ds043991.mongolab.com:43991/mpmongolab');
+mongoose.connect('mongodb://localhost/mightyPlayground');
 var db = mongoose.connection;
 
 module.exports = db;
