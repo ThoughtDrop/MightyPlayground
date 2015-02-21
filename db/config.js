ed1ece261c6c42a856a58a603598f12676913fbb
Robert Ing <rmi22186@gmail.com>
2015-02-20 20:39:45 -0800

    debug database

--- db/config.js
+++ db/config.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 // var keys = require('../keys.js');
 
 if (process.env.PORT) {
-  mongoose.connect('mongodb://' + process.env.mongolabsuser + ':' + process.env.mongolabspassword + '@ds043991.mongolab.com:43991/mpmongolab');
+  // mongoose.connect('mongodb://' + process.env.mongolabsuser + ':' + process.env.mongolabspassword + '@ds043991.mongolab.com:43991/mpmongolab');
+  mongoose.connect('mongodb://' + 'mightyplayground' + ':' + 'hr23thesis' + '@ds043991.mongolab.com:43991/mpmongolab');
 } else {
   mongoose.connect('mongodb://localhost/mightyPlayground');
 }
