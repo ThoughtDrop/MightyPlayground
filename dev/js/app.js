0fa061d58a32635e5ecaee437ca04942fbfe397a
Henry Wong <core.safecrlf>
2015-02-21 16:51:25 -0800

    (housekeeping) new clone

--- dev/js/app.js
+++ dev/js/app.js
@@ -21,7 +21,8 @@ angular.module('thoughtdrop', ['ionic', 'thoughtdrop.controllers', 'thoughtdrop.
 
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  $ionicConfigProvider.tabs.position('bottom');
 // Ionic uses AngularUI Router which uses the concept of states. Learn more here: https://github.com/angular-ui/ui-router
   $stateProvider
 
