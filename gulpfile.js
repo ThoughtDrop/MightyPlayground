d8a82147c41b849cae17a939076431d4fc2ce83b
Peter Kim <chong.peter.kim@gmail.com>
2015-02-23 11:37:11 -0800

    Add more console logs to server

--- gulpfile.js
+++ gulpfile.js
@@ -97,12 +97,12 @@ gulp.task('clean', function(){
 
 gulp.task('inject1', function(){
   gulp.src('www/js/messageController.js')
-    .pipe(replace('//base', "'https://mightyplayground.herokuapp.com' +"))
+    .pipe(replace('//base', "'https://mightyplayground-test.herokuapp.com' +"))
     .pipe(gulp.dest('www/js'));
 });
 gulp.task('inject2', function(){
   gulp.src('www/js/services.js')
-    .pipe(replace('//base', "'https://mightyplayground.herokuapp.com' +"))
+    .pipe(replace('//base', "'https://mightyplayground-test.herokuapp.com' +"))
     .pipe(gulp.dest('www/js'));
 });
 
