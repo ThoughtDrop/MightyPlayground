c16d13033e3c143fdb50d4f842ba3286876e4731
Robert Ing <rmi22186@gmail.com>
2015-02-23 17:01:21 -0800

    (feat) Add testing

---
+++ test/unit/routeSpec.js
@@ -0,0 +1,32 @@
+describe('Routing', function () {
+  // var $route;
+  // beforeEach(module('thoughtdrop'));
+
+  // beforeEach(inject(function($injector){
+  //   $route = $injector.get('$route');
+  // }));
+
+  // it('Should have /signup route, template, and controller', function () {
+  //   expect($route.routes['/login']).to.be.ok();
+  //   expect($route.routes['/login'].controller).to.be('AuthCtrl');
+  //   xexpect($route.routes['/login'].templateUrl).to.be('/auth/signup.html');
+  // });
+
+  // it('Should have /signin route, template, and controller', function () {
+  //   expect($route.routes['/signin']).to.be.ok();
+  //   expect($route.routes['/signin'].controller).to.be('AuthController');
+  //   expect($route.routes['/signin'].templateUrl).to.be('app/auth/signin.html');
+  // });
+
+  // it('Should have /links route, template, and controller', function () {
+  //   expect($route.routes['/links']).to.be.ok();
+  //   expect($route.routes['/links'].controller).to.be('LinksController');
+  //   expect($route.routes['/links'].templateUrl).to.be('app/links/links.html');
+  // });
+
+  // it('Should have /shorten route, template, and controller', function () {
+  //   expect($route.routes['/shorten']).to.be.ok();
+  //   expect($route.routes['/shorten'].controller).to.be('ShortenController');
+  //   expect($route.routes['/shorten'].templateUrl).to.be('app/shorten/shorten.html');
+  // });
+});
