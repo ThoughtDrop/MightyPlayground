5ad026d0742a4b1eab7ecfebb76c6bab4258c4ed
Robert Ing <rmi22186@gmail.com>
2015-02-26 10:57:29 -0800

    (housekeeping) merge conflict with database

--- db/models/messages.js
+++ db/models/messages.js
@@ -6,10 +6,7 @@ var User = require('./user');
 var messagesSchema = new mongoose.Schema({
   _id: Number,
   photo_url: String,
-<<<<<<< HEAD
-=======
   created_at: { type: Date },
->>>>>>> 9606dd42f74d3543426295b3e002355b6320fde9
   // recipient: [{type: Number, ref: 'User'}],
   location : {
     type: { 
