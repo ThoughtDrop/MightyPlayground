6cd8b92de7d779bad5ed933f5b628b95188b59ea
Henry Wong <core.safecrlf>
2015-02-27 15:16:48 -0800

    (bug) search injected into map

--- dev/js/mapController.js
+++ dev/js/mapController.js
@@ -23,7 +23,7 @@ angular.module('thoughtdrop.mapController', [])
           // Create the search box and link it to the UI element.
           var input = /** @type {HTMLInputElement} */(
               document.getElementById('pac-input'));
-          map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+          // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
           var searchBox = new google.maps.places.SearchBox(
             /** @type {HTMLInputElement} */(input));
