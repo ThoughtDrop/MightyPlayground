696ee8b6dbcd8082f5688ba703443e9a7aecc94c
Henry Wong <core.safecrlf>
2015-02-27 21:17:46 -0800

    (feat) maps fix css full height

--- dev/js/mapController.js
+++ dev/js/mapController.js
@@ -2,6 +2,7 @@ angular.module('thoughtdrop.mapController', ['uiGmapgoogle-maps'])
 
 .controller('mapController', function ($scope, $log) {
         //$scope.map.center points to the center all the time. 
+        // TODO: factory passes in the initial latt and long!!!
         $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 15, mapTypeId: google.maps.MapTypeId.ROADMAP};
         $scope.options = {scrollwheel: false};
         var events = {
@@ -10,9 +11,6 @@ angular.module('thoughtdrop.mapController', ['uiGmapgoogle-maps'])
             $scope.map = {center: {latitude: position.k, longitude: position.D }, zoom: 18};
           }
         }
-        // console.log($scope.map);
-
-          // console.log(searchBox);
         $scope.searchbox = { template:'searchbox.tpl.html', events:events};
 
   });
