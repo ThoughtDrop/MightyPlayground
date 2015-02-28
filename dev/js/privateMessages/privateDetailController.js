417f1303934028bbdc156da9ed6383846f4613ae
Peter Kim <chong.peter.kim@gmail.com>
2015-02-28 12:40:44 -0800

    (bug) Server testing

--- dev/js/privateMessages/privateDetailController.js
+++ dev/js/privateMessages/privateDetailController.js
@@ -4,8 +4,8 @@ angular.module('thoughtdrop.privateDetailController', [])
   console.log('ID: ' + $stateParams._id);
   $scope.message = PrivateDetail.get($stateParams._id);
   console.log('Object in here is: ' + JSON.stringify($scope.message));
-  // var creator = 'p3tuh' //ONLY FOR TESTING
-  var creator = $localStorage.userInfo.name;
+  var creator = 'p3tuh' //ONLY FOR TESTING
+  // var creator = $localStorage.userInfo.name;
 
   $scope.addReply = function(replyText, messageID) { //store this reply message as an array inside the individual private messages for simplicity
 
@@ -16,9 +16,9 @@ angular.module('thoughtdrop.privateDetailController', [])
       // picture: $localStorage.userInfo.picture //store picture later
     };
 
-    console.log('reply data: ' + JSON.stringify(reply));
-    $scope.message.replies.push(reply);
+    // console.log('reply data: ' + JSON.stringify(reply));
     PrivateDetail.saveReply(reply); //server req to save this reply to the initial message
+    $scope.message.replies.push(reply); //pushes to $scope for instant rendering
     $scope.message.text = ''; 
     console.log($scope.message.replies);
   };
