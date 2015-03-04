08818aeb918b27bc697e4dc0da26af75d7736ef3
Henry Wong <core.safecrlf>
2015-03-04 15:26:03 -0800

    (feat) completed dynamic radius

--- dev/js/mapController.js
+++ dev/js/mapController.js
@@ -2,7 +2,8 @@ angular.module('thoughtdrop.mapController', [])
 
 .controller('mapController', function($scope, $log, Private, Geolocation) {
   var global_lat, global_lon;
-
+  var cityCircle;
+  $scope.coverage;
   // renders google map
   function initialize () {
     var map_canvas = document.getElementById('map-canvas');
@@ -16,16 +17,38 @@ angular.module('thoughtdrop.mapController', [])
       zoomControl: false,
       mapTypeControl: false
     }
-    map = new google.maps.Map(map_canvas, map_options)
+
+    var map = new google.maps.Map(map_canvas, map_options)
     //WORKS!!!!
     // setInterval(function(){
     //   console.log(map.getCenter());
     // }, 5000);
     //************** CLICK LISTENER ****************//
-    google.maps.event.addListener(map, 'click', function (event) {
-        var latitude = event.latLng.lat();
-        var longitude = event.latLng.lng();
-    });
+    // google.maps.event.addListener(map, 'click', function (event) {
+    //     var latitude = event.latLng.lat();
+    //     var longitude = event.latLng.lng();
+    // });=
+
+  //************** GEO FENCE OVERLAY *****************//
+  // var circle = {
+  //   strokeColor: '#3371F4',
+  //   strokeOpacity: 0.8,
+  //   strokeWeight: 2,
+  //   fillColor: '#94EAFD',
+  //   fillOpacity: 0.50,
+  //   map: map,
+  //   center: map.getCenter(), //center needs to be dynamic Angulardatabinding. 
+  //   radius: 100
+
+  // };
+  // cityCircle = new google.maps.Circle(circle);
+
+  // var myTitle = document.createElement('h1');
+  // myTitle.style.color = 'red';
+  // myTitle.innerHTML = 'coverage: {{coverage}}'+ "m";
+  // var myTextDiv = document.createElement('div');
+  // myTextDiv.appendChild(myTitle);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(myTextDiv);
 
   //************** SEARCH BAR FUNCTIONALITY ****************//
     var input = (document.getElementById('pac-input'));
@@ -54,9 +77,70 @@ angular.module('thoughtdrop.mapController', [])
       var bounds = map.getBounds();
       searchBox.setBounds(bounds);
     });
+
+    // brokedown panBy function. 
+    function offsetCenter(latlng,offsetx,offsety) {
+      // latlng is the apparent centre-point
+      // offsetx is the distance you want that point to move to the right, in pixels
+      // offsety is the distance you want that point to move upwards, in pixels
+      // offset can be negative
+      // offsetx and offsety are both optional
+      var scale = Math.pow(2, map.getZoom());
+      var nw = new google.maps.LatLng(
+          map.getBounds().getNorthEast().lat(),
+          map.getBounds().getSouthWest().lng()
+      );
+      var worldCoordinateCenter = map.getProjection().fromLatLngToPoint(latlng);
+      var pixelOffset = new google.maps.Point((offsetx/scale) || 0,(offsety/scale) ||0)
+      var worldCoordinateNewCenter = new google.maps.Point(
+          worldCoordinateCenter.x - pixelOffset.x,
+          worldCoordinateCenter.y + pixelOffset.y
+      );
+      var newCenter = map.getProjection().fromPointToLatLng(worldCoordinateNewCenter);
+      return newCenter;
+      //map.setCenter(newCenter);
+    }// offsetCenter
+
+
+    console.log('center'+ map.getCenter());
+    // convert LNG/LAT into distance
+    function measure(lat1, lon1, lat2, lon2){  // generally used geo measurement function
+        var R = 6378.137; // Radius of earth in KM
+        var dLat = (lat2 - lat1) * Math.PI / 180;
+        var dLon = (lon2 - lon1) * Math.PI / 180;
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        return d * 1000; // meters
+    }
+    function getMeters(){
+      var center = map.getCenter();
+      console.log("got here");
+      var offCenter = offsetCenter(map.getCenter(), 53, 0);
+      var distance = measure(center.lat(), center.lng(), offCenter.lat(), offCenter.lng());
+      console.log('center: ' + center, 'offCenter: ' + offCenter + 'distance: '+ distance);
+      return distance;
+    }
+
+
     $scope.clearText = function(){
+       getMeters();
+       // var center = map.getCenter();
+       // var offCenter = offsetCenter(map.getCenter(), 53, 0);
+       // var distance = measure(center.lat(), center.lng(), offCenter.lat(), offCenter.lng());
+       // console.log('distance: '+ distance);
+
       document.getElementById('pac-input').value = '';
     }
+
+
+    $scope.coverage = 100;
+    google.maps.event.addListener(map, 'idle', function(){
+      $scope.coverage = getMeters();//offsetCenter(map.getCenter(), 53, 0);
+      $scope.$apply();
+    });
     // $scope.alert = function(){
     //   alert(map.getCenter());
     // };
