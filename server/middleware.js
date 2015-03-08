f57c8813b51991a157b7417afff9b3fa137e5f41
Robert Ing <rmi22186@gmail.com>
2015-03-07 16:30:04 -0800

    (fix) CSS for headings

--- server/middleware.js
+++ server/middleware.js
@@ -12,7 +12,7 @@ module.exports = function (app, express) {
   app.use(morgan('dev')); 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + './../landingPage'));
+  app.use(express.static(__dirname + './../dev'));
 
   app.use('/api/auth', authRouter); // use user router for all auth request
   app.use('/api/messages', messageRouter); // use message router for all message requests
